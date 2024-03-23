dc950ed108aa5e4b1efc06164712d41076dfca26
Lukas Grossberger <code@grossberger.xyz>
2024-03-23 14:44:08 +0100

    upload broken links report as artifact, fix script, run as matrix

--- check_broken_links.sh
+++ check_broken_links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget --spider -r -nd -nv -H -l 2 -w 1 -o /tmp/coagulate_broken_links.log $1
 
-BROKEN=$(egrep -B1 'broken link!' coagulate_broken_links.log)
+BROKEN=$(egrep -B1 'broken link!' /tmp/coagulate_broken_links.log)
 
 if [ -z "$BROKEN" ]; then
     echo "Success! No broken links detected."
