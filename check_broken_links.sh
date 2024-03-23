7c3abb74ae58c7b814abb6b6e00e6b890c733f89
Lukas Grossberger <code@grossberger.xyz>
2024-03-23 15:01:09 +0100

    bring back error condition to broken link check

--- check_broken_links.sh
+++ check_broken_links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget --spider -r -nd -nv -H -l 1 -w 1 -o /tmp/coagulate_broken_links.log $1
 
-BROKEN=$(egrep -B1 'broken link!' /tmp/coagulate_broken_links.log)
+BROKEN=$(egrep -B1 'broken link!|ERROR' /tmp/coagulate_broken_links.log)
 
 if [ -z "$BROKEN" ]; then
     echo "Success! No broken links detected."
